ab5186d22e6042278cd328242636de73a4d73628
Seungoh <styu12@naver.com>
2022-03-17 19:00:04 +0900

    Upload Movie

--- src/movieController.js
+++ src/movieController.js
@@ -3,6 +3,7 @@ import {
   getMovies,
   getMovieByMinimumRating,
   getMovieByMinimumYear,
+  addMovie,
 } from "./db";
 
 export const home = (req, res) => {
@@ -10,11 +11,11 @@ export const home = (req, res) => {
   return res.render("home", { pageTitle: "Movies!", movies });
 };
 
-export const movieDetail = (req, res) => {
+export const movieDetail = async (req, res) => {
   const { id } = req.params;
-  const movie = getMovieById(id);
+  const movie = await getMovieById(id);
   return res.render("detail", {
-    pageTitle: movie.title,
+    pageTitle: "Detail",
     movie,
   });
 };
@@ -34,4 +35,18 @@ export const filterMovie = (req, res) => {
     pageTitle = "Movies!";
   }
   return res.render("filter", { pageTitle, movies });
+};
+
+export const getUpload = (req, res) => {
+  return res.render("upload", { pageTitle: "Upload Movie" });
+};
+
+export const postUpload = (req, res) => {
+  const { title, synopsis, genre } = req.body;
+  const genres = genre.split(",");
+  console.log(title, synopsis, genres);
+  const data = { title, synopsis, genres };
+  console.log(data);
+  addMovie(data);
+  return res.redirect("/");
 };
--- src/movieRouter.js
+++ src/movieRouter.js
@@ -1,9 +1,16 @@
 import express from "express";
-import { home, movieDetail, filterMovie } from "./movieController";
+import {
+  home,
+  movieDetail,
+  filterMovie,
+  getUpload,
+  postUpload,
+} from "./movieController";
 
 const movieRouter = express.Router();
 movieRouter.get("/", home);
 movieRouter.get("/filter", filterMovie);
+movieRouter.route("/add").get(getUpload).post(postUpload);
 movieRouter.get("/:id", movieDetail);
 
 export default movieRouter;
